eebce5ae1716946ff279354a3b51b8557087d77b
william-dvlpr <william.dvlpr@gmail.com>
2020-06-18 03:45:27 -0300

    convert bin 2 dec

--- dec2bin.py
+++ dec2bin.py
@@ -1,12 +1,26 @@
 # coding: UTF-8
 
-decimal = int(input('Enter decimal number: '))
+reply = input('[1] - decimal to binary\n[2] - binary to decimal\n')
 
-binary = "" 
+if reply == '1':
+    decimal = int(input('Enter decimal number: '))
+    binary = "" 
+    while decimal >= 1 :
+        remainder = decimal % 2
+        binary = str(remainder) + binary # inverted order
+        decimal = decimal // 2
 
-while decimal >= 1 :
-    remainder = decimal % 2
-    binary = str(remainder) + binary # inverted order
-    decimal = decimal // 2
+    print('Converted to binary:', binary)
 
-print("Converted to binary: ", binary)
+elif reply == '2':
+    binary = input('Enter binary number: ')
+    position = len(binary) - 1
+    decimal = 0
+    for i in binary:
+        decimal += int(i) * 2**position
+        position -= 1
+
+    print('Converted to decimal:', decimal)    
+
+else:
+    print('Invalid input') 
